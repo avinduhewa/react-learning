154eb82b685d172d70295271517edf04e35dea88
avinduhewa <avinduhewa@gmail.com>
2017-01-20 10:43:02 +0530

    adding eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
-var path = require('path');
+const debug = process.env.NODE_ENV !== 'production'; 
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-  context: path.join(__dirname, "src"),
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/client.js",
+  context: path.join(__dirname, 'src'),
+  devtool: debug ? 'inline-sourcemap' : null,
+  entry: './js/client.js',
   module: {
     loaders: [
       {
@@ -14,14 +14,18 @@ module.exports = {
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
-        }
-      }
-    ]
+          plugins: [
+            'react-html-attrs',
+            'transform-decorators-legacy',
+            'transform-class-properties'
+          ],
+        },
+      },
+    ],
   },
   output: {
-    path: __dirname + "/src/",
-    filename: "client.min.js"
+    path: path.join(__dirname, '/src/'),
+    filename: 'client.min.js',
   },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
